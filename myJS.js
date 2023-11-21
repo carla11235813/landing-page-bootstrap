04d4c70ae05acfbb0608760b33fa8b0657107c69
carla11235813 <111895486+carla11235813@users.noreply.github.com>
2023-11-21 16:06:19 -0300

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -34,7 +34,7 @@ function moverHTML() {
             
                 // Crie o HTML que deseja adicionar
             var novoHTML = '\
-                <a href="#" class="nav-link">Login/Cadastro</a>\
+                <a href="tela-de-login/login.html" class="nav-link">Login/Cadastro</a>\
                 <a href = "#" class="nav-link">Carinho</a> ';
 
                 // Adicione o novo HTML ao elemento alvo
@@ -80,7 +80,7 @@ function moverHTML() {
             // Crie o HTML que deseja adicionar
             var novoHTML = '\
             <div class="navbar-nav me-auto">\
-                <a href="/telas-de-cadastro/opc_cadastro.html" class="nav-link">\
+                <a href="tela-de-login/login.html">\
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">\
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>\
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>\
